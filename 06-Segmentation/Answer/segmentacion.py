fbe8eb304dcc36a2d64817a409fbb0e35cdf8709
nathan <mateomarin97@hotmail.com>
2018-03-05 20:23:58 -0500

    Mezcla de Gaussianas

--- 06-Segmentation/Answer/segmentacion.py
+++ 06-Segmentation/Answer/segmentacion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, color
@@ -41,6 +42,28 @@ def segmentByClustering( rgbImage, featureSpace, clusteringMethod, numberOfClust
 
 
 
+        
+    #Ahora rgb con gmm
+    if featureSpace=='rgb'and clusteringMethod=='gmm':
+        Imagen=rgbImage
+        #Tambien obtenemos la imagen a blanco y negro
+        Imagenbn=rgb2gray(Imagen)
+        #Ahora vamos a representar cada pixel en el espacio rgb mas intensidad, para ello tenemos la lista vectores con todos los vectores
+        vectores=[]
+        for i in range(Imagen.shape[0]):
+            for j in range(Imagen.shape[1]):
+                #aux es el vector de representacion del pixel,va asi, r,g,b,Intensidad
+                aux=[Imagen[i][j][0],Imagen[i][j][1],Imagen[i][j][2],Imagenbn[i][j]]
+                vectores.append(aux)
+        #Ahora que tenemos los puntos pasamos a hacer gmm
+        gmm=GaussianMixture(n_components=k).fit(np.array(vectores))
+        #Ahora obtenemos el mapa de la imagen
+        map=gmm.predict(np.array(vectores))
+        segmentation = map.reshape(Imagen.shape[0],Imagen.shape[1])
+        
+
+
+
     return segmentation
                 
         
@@ -50,7 +73,7 @@ Imagen1 = io.imread(filename)
 #Hagamos varios k
 valoresk=[3]
 for w in valoresk:
-    Segmentacion1=segmentByClustering( Imagen1, 'rgb', 'kmeans', w)
+    Segmentacion1=segmentByClustering( Imagen1, 'rgb', 'gmm', w)
     np.savetxt("Segmentacion"+str(w)+".dat",Segmentacion1)
 
 
